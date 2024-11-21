11ce81972729188d6756f20fac787156884b12fc
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-21 22:29:11 +0600

    Now I am trying to learn Python variables

--- day 1/first.py
+++ day 1/first.py
@@ -5,3 +5,10 @@
 # print(23+23-2/3)
 
 print("Multiple things at a time" , " and have some fun with this!")
+
+# now I am trying to lear variable in python
+name = "Kashem"
+Age = 23
+sum = 23 + 23
+
+print(name , Age , sum)
