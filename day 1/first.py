4a7887e931190dfee5c1aa4c3c41ad4c348fa7b5
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-21 21:37:23 +0600

    print integer number like 23 and print multiple word by single print statement

--- day 1/first.py
+++ day 1/first.py
@@ -1,3 +1,7 @@
-print("Hello world")
-print("My age in 22")
-print("something is better than nothing !")
+# print("Hello world")
+# print("My age in 22")
+# print("something is better than nothing !")
+# print(23)
+# print(23+23-2/3)
+
+print("Multiple things at a time" , " and have some fun with this!")
