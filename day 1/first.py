5272fde032a228c928e693868a334f05f6cadc91
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-21 22:44:45 +0600

    that's all about day 1

--- day 1/first.py
+++ day 1/first.py
@@ -10,14 +10,31 @@ print("Multiple things at a time" , " and have some fun with this!")
 name = "Kashem"
 Age = 23
 sum = 23 + 23
-price = 21.0123
+price = 21.012
+Is_same = True
 # print(name , Age , sum , price+sum)
 
 # this is for check data type in Python
 
-print(type(name))
-print(type(Age))
-print(type(sum))
-print(type(price))
-print(type(print))
-print(type(type))
+# print(type(name))
+# print(type(Age))
+# print(type(sum))
+# print(type(price))
+# print(type(Is_same))
+# print(type(print))
+# print(type(type))
+
+# string save in different wey
+name1 = 'Kashem'
+name2 = "Kashem"
+name3 = '''Kashem'''
+
+# bool data type have to write the first later capital latter 
+is_same = True        #-- ok
+is_same = False       #-- ok
+# is_same = true      -- this is wrong 
+# is_same = false     -- this is also wrong
+print(is_same)
+print(name1)
+print(name2)
+print(name3)
