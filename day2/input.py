8f158db6120341a5eb5aa902a758977a35f47f97
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-22 09:32:34 +0600

    Input in Python and change data type

---
+++ day2/input.py
@@ -0,0 +1,9 @@
+# how to take a input in Python
+# by default when we input values in Python is "string"
+
+x = int(input("input x value :" ))
+y = float( input("input Y value: ") )
+
+print(type(x))
+print(type(y))
+print(x + y)
