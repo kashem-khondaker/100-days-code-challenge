c8e28778335ab29dad64b042a77e09e3670891fe
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-22 09:13:43 +0600

    Learn operator : assign operator

--- day2/learnPython.py
+++ day2/learnPython.py
@@ -4,12 +4,38 @@
 a = 20
 b = 3
 
-print(a+b)   
-print(a-b)  
-print(a*b)  
-print(a/b)
-print(a%b)
-print(a**b)
+# print(a+b)   
+# print(a-b)  
+# print(a*b)  
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# assign operator ( = , += , -= , *= , /= ,%= , **=)
+
+x = 10
+# x = x +10
+x += 10
+
+print(x)
+
+x -= 10
+print(x)
+
+x *= 2
+print(x)
+
+x /= 2
+print(x)
+
+x %=2
+print(x)
+
+x **= 2
+print(x)
 
 
+
+
+# logical operator (and , or , not)
 
