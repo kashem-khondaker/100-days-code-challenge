971782e46234f49c079fde58db92182e96c3ca3a
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-23 10:04:47 +0600

    some useful functions of string in Python

--- day 3/string.py
+++ day 3/string.py
@@ -1,8 +1,29 @@
-name = "kashem khonddaker"
+# name = "kashem khonddaker"
 
 #this is called string slicing
 
-print(name[1:4])
-print(name[2:])
-print(name[:len(name)])
+# print(name[1:4])
+# print(name[2:])
+# print(name[:len(name)])
+
+
+# some useful function in Python 
+
+text = "apple , banana,cherry"
+fruits = text.split(",")
+print(fruits)
+
+name = "md. kashem khondaker"
+print(len(name))
+
+print(name.upper())
+print(name.lower())
+
+print(name.startswith("m"))
+print(name.endswith("er"))
+
+print("/n", name.startswith("k"))
+print("/n", name.endswith("i"))
+
+
 
