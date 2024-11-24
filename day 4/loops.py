b4f46867eddb6f85ef6e869dcc6119116afc8fcb
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-24 15:23:14 +0600

    comment and uncomment my code

--- day 4/loops.py
+++ day 4/loops.py
@@ -1,75 +1,76 @@
-# x = 25
-# y = 26
-# if(x>y) :
-#     print('True')
-#     if(x*2 == 50):
-#         print('True')
-# else:
-#     print('Not True')
+x = 25
+y = 26
+
+if(x>y) :
+    print('True')
+    if(x*2 == 50):
+        print('True')
+else:
+    print('Not True')
 
 # for loop in Python
 
 #          for item in sequence:
-    #           code to execute
+#               code to execute
     
     
 # basic loop in a array
     
-# fruits = ["apple" , "banana" , "cherry"]
-# print(type(fruits))
+fruits = ["apple" , "banana" , "cherry"]
+print(type(fruits))
 
-# for fruit in fruits:
-#     print(fruit)
+for fruit in fruits:
+    print(fruit)
 
 
 # Looping over a range()
 
 
 
-# print("Looping value with range()\n")
-# for value in range(10):
-#     print(value)
+print("Looping value with range()\n")
+for value in range(10):
+    print(value)
 
 
-# # Looping over a range() with advance
+# Looping over a range() with advance
 
 
-# print("\nLooping value advanced with range() \n")    
-# for value in range(1,11,2):
-#     print(value)
+print("\nLooping value advanced with range() \n")    
+for value in range(1,11,2):
+    print(value)
 
 
 
-# # nested for LOOPS in Python
+# nested for LOOPS in Python
 
-# print("\nNested FOR loop Python\n")
-# matrix = [
-#     [1,2,3],
-#     [4,5,6],
-#     [7,8,9]
-# ]
+print("\nNested FOR loop Python\n")
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
 
-# for row in matrix:
-#     for num in row:
-#         print(num , end=" ")
-#     print()
+for row in matrix:
+    for num in row:
+        print(num , end=" ")
+    print()
 
-# # Loop with if , else condition 
+# Loop with if , else condition 
 
-# print("\nLoop with condition\n")
+print("\nLoop with condition\n")
 
-# for i in range(5):
-#     print(i)
-# else:
-#     print("Loop completed!")
+for i in range(5):
+    print(i)
+else:
+    print("Loop completed!")
 
-# print("\nanother conditional for loop in Python\n")
+print("\nanother conditional for loop in Python\n")
 
-# for i in range(10):
-#     if(i%2==0):
-#         print(i)
-#     else:
-#         continue
+for i in range(10):
+    if(i%2==0):
+        print(i)
+    else:
+        continue
 
 print("\npractice with some if else condition in for loop\n")
 
