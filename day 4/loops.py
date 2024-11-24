327a9856ce4f86220d05856164aa798b2ece43ef
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-24 15:21:31 +0600

    print their index with value by for loop in Python

--- day 4/loops.py
+++ day 4/loops.py
@@ -26,48 +26,68 @@
 
 
 
-print("Looping value with range()\n")
-for value in range(10):
-    print(value)
+# print("Looping value with range()\n")
+# for value in range(10):
+#     print(value)
 
 
-# Looping over a range() with advance
+# # Looping over a range() with advance
 
 
-print("\nLooping value advanced with range() \n")    
-for value in range(1,11,2):
-    print(value)
+# print("\nLooping value advanced with range() \n")    
+# for value in range(1,11,2):
+#     print(value)
 
 
 
-# nested for LOOPS in Python
+# # nested for LOOPS in Python
 
-print("\nNested FOR loop Python\n")
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
+# print("\nNested FOR loop Python\n")
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
 
-for row in matrix:
-    for num in row:
-        print(num , end=" ")
-    print()
+# for row in matrix:
+#     for num in row:
+#         print(num , end=" ")
+#     print()
 
-# Loop with if , else condition 
+# # Loop with if , else condition 
 
-print("\nLoop with condition\n")
+# print("\nLoop with condition\n")
 
-for i in range(5):
-    print(i)
-else:
-    print("Loop completed!")
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Loop completed!")
 
-print("\nanother conditional for loop in Python\n")
+# print("\nanother conditional for loop in Python\n")
 
-for i in range(10):
-    if(i%2==0):
-        print(i)
+# for i in range(10):
+#     if(i%2==0):
+#         print(i)
+#     else:
+#         continue
+
+print("\npractice with some if else condition in for loop\n")
+
+for i in range(20):
+    if i <= 10:
+        if i % 2 == 0 and i != 0:
+            print(f"i = {i}", end="\n")  # Print i when i is even and not 0
     else:
-        continue
+        print("Fuck you!")
+        break  # Break the loop when i > 10
+
+# print their index with value
+
+for i in range(20):
+    if i <= 10:
+        if i % 2 == 0 and i != 0:
+            print(f'Index = {i}, Value = {i}', end="\n")
+    else:
+        print(f'Index = {i}, Value = Fuck you!')
+        break
 
---
+++ day 4/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+for i in range(20):
+    if i <= 10:
+        if i % 2 == 0 and i != 0:
+            print(f'Index = {i}, Value = {i}', end="\n")
+    else:
+        print(f'Index = {i}, Value = Fuck you!')
+        break
