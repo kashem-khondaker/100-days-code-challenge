ee32b0904cc03192944eb41d245772be45ea93b0
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-26 18:49:12 +0600

    Learn some function in Python

---
+++ day 6/build_in_function.py
@@ -0,0 +1,15 @@
+highest = max(1,12,124,32,321) # simple number will be work
+highest2 = max([1,12,124,32,-321]) # array with be work
+lowest =  min(21,31,312)
+lowest2 =  min([0,21,31,312])
+
+sum1 = sum([123,321,2,1,21,33])
+
+long_name = 'this is a string '
+
+print(highest)
+print(highest2)
+print(lowest)
+print(lowest2)
+print(len(long_name))
+print(sum1)
