8d39d0336f14e79d410097ad5eeb0fc950f0f08f
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-11-27 07:55:49 +0600

    Learn some list function like append , pop in Python

---
+++ day 7/cpp.cpp
@@ -0,0 +1,6 @@
+// for (int i = 0; i < n; i++)
+// {
+//     int x;
+//     cin >> x;
+//     v.push_back(x);
+// }
---
+++ day 7/list_something.py
@@ -0,0 +1,26 @@
+
+# n = int(input("Enter the number of elements: "))  # Read the number of elements
+# v = []  # Initialize an empty list
+
+# for i in range(n):
+#     x = int(input())  # Read an integer
+#     v.append(x)  # Append the integer to the list
+
+# print(v)  # Optional: Print the list to verify
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+print(stack)
+stack.pop()
+print(stack)
+
+
+
+values = []
+values.append('x')
+print(values)
+
+
+
+
+    
