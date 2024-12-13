48d23195a509059518f14325e47e451bbcdd44f4
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-13 10:01:20 +0600

    create menu file and create menu class

--- day 19/menu.py
+++ day 19/menu.py
@@ -1,2 +1,31 @@
 class Menu():
-    pass
+    def __init__(self):
+        self.items = []
+        
+    def add_menu_item(self , item):
+        self.items.append(item)
+        
+        
+    def find_item(self , item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+        return None
+    
+    def remove_item(self , item_name):
+        item = self.find_item(item_name)
+        
+        if item :
+            self.items.remove(item)
+            print(f'{item_name} deleted')
+        else:
+            print(f'{item_name} not found')
+            
+    def show_menu(self):
+        print('\n---- Menu-----')
+        print('Name\tPrice\tQuantity')
+        
+        for item in self.items:
+            print(f'{self.name}\t{self.price}\t{self.quantity}')
+            
+    
