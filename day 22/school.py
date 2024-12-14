0e90c77c9273f4fe050d6f873852029d2d1f078e
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-14 11:22:44 +0600

    calculate  value to grade funtion implement

--- day 22/school.py
+++ day 22/school.py
@@ -32,6 +32,24 @@ class School:
             return 'D'
         else:
             return 'F'
+        
+    @staticmethod
+    def value_to_grade(value):
+        if value >= 4.5 and value <= 5.00:
+            return 'A+'
+        elif value >= 4.00 and value < 4.50:
+            return 'A'
+        elif value >= 3.50 and value < 4.00:
+            return 'A-'
+        elif value >= 3.00 and value < 3.50:
+            return 'B'
+        elif value >= 2.50 and value < 3.00:
+            return 'C'
+        elif value >= 2.00 and value < 2.50:
+            return 'D'
+        else:
+            return 'F'
+        
     
     @staticmethod
     def grade_to_value(grade):
