87090afb72a743acef4510672c5e92602f7baeea
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-31 10:59:08 +0600

    Learn object in js

---
+++ day 37/object_in.js
@@ -0,0 +1,22 @@
+var obj={
+    name : "kashem khondaker",
+    age : 20,
+    edu : "High School",
+    sex : "male"
+};
+
+console.log(obj)
+console.log(obj.name , obj.edu);
+
+var person = {
+    name : "rakib",
+    sex : "male" , 
+    edu : "Hon's final year ",
+    age : 24 , 
+    hoby : {
+        video : "watching sex video .",
+        listen : 'Song of nora fatehi '
+    }
+}
+
+console.log(person.hoby)
