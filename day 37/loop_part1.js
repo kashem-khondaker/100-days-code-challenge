667a2aa14bd17027b42f6e92a8196f8ffa1417d2
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-31 19:00:11 +0600

    Learn bassic loop part 1

---
+++ day 37/loop_part1.js
@@ -0,0 +1,8 @@
+var images = ["image1" , "images2" , "images3" , "deleted images" , 12 , 24 , 76];
+// console.log(images);
+
+
+for (let i = 0; i < images.length; i++) {
+    const element = images[i];
+    console.log(element);
+}
