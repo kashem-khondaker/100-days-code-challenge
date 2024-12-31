9b3bb35159f6a73653de73cf025bd6bc92f877aa
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-31 10:03:23 +0600

    data type done

---
+++ day 37/data_type.js
@@ -0,0 +1,12 @@
+var test = 20;
+var test2 = 'kashem khondaker';
+var bol = true;
+
+var obj = {
+    name: 'kabila',
+};
+
+console.log(typeof test);
+console.log(typeof test2);
+console.log(typeof bol);
+console.log(typeof obj);
