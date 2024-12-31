450332f33baba3d5a03faa806f026482ee055f78
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-31 19:41:45 +0600

    Learn string dynamic template and spread operator

--- day 37/app.js
+++ day 37/app.js
@@ -5,3 +5,18 @@ var cursor = 'pointer'
 console.log(cursor)
 console.log(node)
 console.log(test)
+
+
+// string dynamic templet litarels and spread operator
+
+const name = "kashem";
+const str = `My name is ${name}`;
+console.log(str);
+
+// spread operator 
+
+const num = [1,2,3 , 4 ,5 ,6];
+const num2 = [11 , 12 , 13 , 14];
+// console.log([num , num2]);
+// using spread operator 
+console.log([...num , ...num2]);
