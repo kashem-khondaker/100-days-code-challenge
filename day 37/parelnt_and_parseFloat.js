593994c53215d8ee0249600325242e13d5a01de7
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-31 10:19:34 +0600

    Learn how to convert number int and float

---
+++ day 37/parelnt_and_parseFloat.js
@@ -0,0 +1,13 @@
+var nam = 12.5;
+var string = '12.3'
+var convert_number = parseInt(string);
+var convert_number2 = parseFloat(string);
+
+var nam2 = 9;
+
+var result = nam + nam2 + convert_number + convert_number2;
+
+console.log(typeof nam);
+console.log(typeof nam2);
+console.log(typeof result);
+console.log( result);
