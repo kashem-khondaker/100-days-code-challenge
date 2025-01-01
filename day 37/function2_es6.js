493c3e5322aebb050536c8d4abda059644fd74ab
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2025-01-01 07:13:27 +0600

    Learn function 2 in es6

---
+++ day 37/function2_es6.js
@@ -0,0 +1,19 @@
+function sum(num1 , num2) {
+    return num1 + num2;
+};
+
+const result = sum(1,12);
+console.log(result);
+
+const int = (num1 , num3) => {
+    //multiple line work !
+    return num3/num1;
+}
+
+console.log(int(3,9));
+
+// by default return this
+const flot = ( x , y) => x+y;
+console.log(flot);
+const number = flot(5,5);
+console.log(number);
