4fef6aad8471dd8fe19482ba75ba771aab163fa4
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2025-01-03 12:24:04 +0600

    comment input fild

--- day 38/app.js
+++ day 38/app.js
@@ -1,15 +1,24 @@
-const x = document.getElementById("h-title");
+// const x = document.getElementById("h-title");
 // x.document.background='red'
 // console.log(x);
 
-const allbox = document.getElementsByClassName('box');
-console.log(allbox)
-for (let i = 0; i < allbox.length; i++) {
-    const element = allbox[i];
-    console.log(element);
-    element.style.backgroundColor = 'green';
-    if (element.innerText == "box-5") {
-    element.style.backgroundColor = 'red';
-    }
-    
-}
+// const allbox = document.getElementsByClassName('box');
+// console.log(allbox)
+// for (let i = 0; i < allbox.length; i++) {
+//     const element = allbox[i];
+//     console.log(element);
+//     element.style.backgroundColor = 'green';
+//     if (element.innerText == "box-5") {
+//     element.style.backgroundColor = 'red';
+//     }
+// }
+
+document.getElementById("btn").addEventListener
+("click" , (event)=>{
+console.log('hello world in console ...');
+});
+
+// const onclick_function = (event)=>{
+//     console.log('hello world in console ...');
+// }
+
