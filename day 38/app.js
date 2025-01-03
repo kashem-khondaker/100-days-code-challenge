a15054462677761b11d1f642535de8e1bdb32e35
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2025-01-03 10:22:09 +0600

    Learn how to apply css using js

--- day 38/app.js
+++ day 38/app.js
@@ -1,3 +1,15 @@
 const x = document.getElementById("h-title");
 // x.document.background='red'
-console.log(x);
+// console.log(x);
+
+const allbox = document.getElementsByClassName('box');
+console.log(allbox)
+for (let i = 0; i < allbox.length; i++) {
+    const element = allbox[i];
+    console.log(element);
+    element.style.backgroundColor = 'green';
+    if (element.innerText == "box-5") {
+    element.style.backgroundColor = 'red';
+    }
+    
+}
