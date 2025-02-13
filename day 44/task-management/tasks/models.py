69fd0e75ebac03c5893f8a641d75e427a06fc879
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-02-13 08:39:57 +0600

    Signal issue proper fixed , email send perfectly ..

--- day 44/task-management/tasks/models.py
+++ day 44/task-management/tasks/models.py
@@ -83,31 +83,3 @@ class taskDetails(models.Model):
             print(kwargs)
             
             instance.is_completed = True
-    
-# @receiver(post_delete , sender = Task)
-# def delete_associate_details(sender , instance , **kwargs):
-#     if isinstance:
-#         print(instance)
-#         instance.details.delete()
-#         print("deleted successfully ")
-
-@receiver(m2m_changed , sender = Task.employee.through)
-def Notify_employee_on_task_creation(sender , instance , action,**kwargs):        
-    if action=="post_add":
-        assigned_emails = [emp.email for emp in instance.employee.all()]
-        print(assigned_emails)
-        send_mail(
-            "Checkout your new task ",
-            f"You have been assigned to this task : {instance.title}",
-            "kashem.khondaker.official001@gmail.com",
-            assigned_emails,
-            fail_silently=False,   # for show error when main not send !
-        )
-
-# @receiver(post_delete , sender = Task)
-# def delete_associate_details(sender , instance , **kwargs):
-#     if isinstance:
-#         print(instance)
-#         if instance.details:
-#             instance.details.delete()
-#             print("deleted successfully ")
